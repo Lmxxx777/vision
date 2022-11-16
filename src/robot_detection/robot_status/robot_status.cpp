234af2b113ef5a85810bb7a9aa49e04a8701278c
Lmx6771 <2351006771@qq.com>
2022-11-16 22:03:24 +0800

    文件结构及修改
    robot_status增加了cpp文件来初始化参数，为了节点文件中接收电控数据。
    增加了继承关系，让src不用在每一个类里面创建，直接继承state的。
    可能造成cmake文件编写复杂，但是文件结构相较之前美观但层次太多，过几天更改文件结构。

---
+++ src/robot_detection/robot_status/robot_status.cpp
@@ -0,0 +1,24 @@
+#include "robot_status.h"
+
+robot_state::robot_state()
+{
+    enermy_color = BLUE;
+    enermy_type = SMALL;
+}
+
+void robot_state::initSrc(cv::Mat Src_)
+{
+    src=Src_.clone();
+}
+
+void robot_state::initPose(float p, float y, float r)
+{
+    ab_pitch = p;
+    ab_yaw = y;
+    ab_roll = r;
+}
+
+void robot_state::initSpeed(float speed)
+{
+    bullet_speed = speed;
+}
---
+++ src/robot_detection/robot_status/robot_status.h
@@ -0,0 +1,38 @@
+//opencv
+#include <opencv2/opencv.hpp>
+//ros
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+
+//basic parameter
+#define GRAVITY 9.78
+#define SMALL_AIR_K 0.01903
+#define BIG_AIR_K 0.00556
+#define BIG_LIGHT_AIR_K 0.00530
+
+//robot basic classes
+enum EnermyColor { RED = 1, BLUE = 2 };
+enum EnermyType  { SMALL = 1, BIG = 2 };
+enum EnermyState { RUN = 1, SPIN = 2};
+enum SpinHeading { UNKNOWN, CLOCKWISE, COUNTER_CLOCKWISE };
+
+//robot state information from electronic control group
+class robot_state
+{
+public:
+    cv::Mat src;
+
+    //电控发来的角度和弹速
+    float ab_pitch;
+    float ab_yaw;
+    float ab_roll;
+    float bullet_speed;
+    int enermy_color;
+    int enermy_type;
+
+
+    robot_state();
+    void initSrc(cv::Mat Src_);
+    void initPose(float p, float y, float r);
+    void initSpeed(float speed);
+};
